b5bb543fe5ddfcdda82109f6e2e0ebc51b39532e
Rebekah Klemm <rebekah_klemm@yahoo.com>
2016-11-08 11:03:30 -0600

    added use, special, and modernism

--- app.js
+++ app.js
@@ -1,11 +1,37 @@
-const express = require( 'express' );
+const express = require('express');
 const app = express(); // creates an instance of an express application
+
 
 
 app.listen(3000);
 console.log('server listening');
-app.get('/', function(request, response, next)
-    {response.send("hello world")
 
-    });
-    
+// app.use(function (req, res, next) {
+//     req.get('/', function(request, response, next){
+//         console.log('You are in reqGet');
+//     });
+//     // call `next`, or else your app will be a black hole — receiving requests but never properly responding
+//     next();
+// })
+
+app.use(function(req, res, next){
+    console.log('request');
+    next();
+})
+
+app.use('/special/', function(req, res, next){
+    console.log('you are in special');
+    next();
+})
+
+app.post('/modernism', function(request, response){
+    console.log('we are posting!');
+})
+
+app.get('/', function (request, response) {
+    response.send('hello world')
+})
+
+app.get('/news', function(request, response){
+    response.send('this is the news page');
+})
